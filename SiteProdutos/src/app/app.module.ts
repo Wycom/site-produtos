8706cb17b2e2d9180c8136dbc514c90c298cab0e
António Ribeiro <a.j.a.rbr@gmail.com>
2019-09-10 15:46:13 +0100

    Criar Componentes

--- SiteProdutos/src/app/app.module.ts
+++ SiteProdutos/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { PaginaInicialComponent } from './pagina-inicial/pagina-inicial.component';
+import { PaginaProdutoComponent } from './pagina-produto/pagina-produto.component';
+import { FormContactosComponent } from './form-contactos/form-contactos.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    PaginaInicialComponent,
+    PaginaProdutoComponent,
+    FormContactosComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ SiteProdutos/src/app/form-contactos/form-contactos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-contactos',
+  templateUrl: './form-contactos.component.html',
+  styleUrls: ['./form-contactos.component.css']
+})
+export class FormContactosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
